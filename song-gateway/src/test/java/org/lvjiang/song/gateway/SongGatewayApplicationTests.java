a9b59fa0a2f7aacf9c55558963b7cf8c5ec3f795
wang fan <fan.wang02@weimob.com>
2024-08-12 00:43:56 +0800

    gateway-init & logback

---
+++ song-gateway/src/test/java/org/lvjiang/song/gateway/SongGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lvjiang.song.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
