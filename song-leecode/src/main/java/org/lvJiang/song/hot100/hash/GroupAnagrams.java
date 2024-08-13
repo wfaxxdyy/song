b72a1a42deb983a63af7b3e96e8f6f7fafdbed6d
Evan.Wang <wfaxxdyy@163.com>
2024-08-13 21:24:30 +0800

    leecode

---
+++ song-leecode/src/main/java/org/lvJiang/song/hot100/hash/GroupAnagrams.java
@@ -0,0 +1,55 @@
+package org.lvJiang.song.hot100.hash;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+ * 字母异位词 是由重新排列源单词的所有字母得到的一个新单词。
+ * 示例 1:
+ *
+ * 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ * 示例 2:
+ *
+ * 输入: strs = [""]
+ * 输出: [[""]]
+ * 示例 3:
+ *
+ * 输入: strs = ["a"]
+ * 输出: [["a"]]
+ *
+ *
+ * 提示：
+ *
+ * 1 <= strs.length <= 104
+ * 0 <= strs[i].length <= 100
+ * strs[i] 仅包含小写字母
+ */
+public class GroupAnagrams {
+    public static void main(String[] args) {
+        List<List<String>> lists = groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
+        System.out.println(lists.toString());
+    }
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        if (strs == null || strs.length == 0) {
+            return result;
+        }
+        Map<String, List<String>> map = new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+            char[] charArray = strs[i].toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+            if (!map.containsKey(sortedStr)) {
+                List<String> list = new ArrayList<>();
+                list.add(strs[i]);
+                map.put(sortedStr, list);
+            } else {
+                map.get(sortedStr).add(strs[i]);
+            }
+        }
+        result.addAll(map.values());
+        return result;
+    }
+}
